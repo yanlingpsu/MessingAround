8d691c1cea285827a04a6de82e2b03aed0ba3776
Yanling Wang <yuw17@psu.edu>
2020-10-07 11:39:48 +0000

    lab5 commit 1

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -12,6 +12,7 @@ def is_palindrome1(s):
   You must implement this with a while loop that compares
   characters in s one by one going forward vs. going backward
   """
+  return False
   return True
 
 def is_palindrome2(s):
@@ -24,6 +25,7 @@ def is_palindrome2(s):
   Use negative index to get the last character and use
   slicing to get the middle of the string.
   """
+  return False
   return True
 
 def is_palindrome3(s):
@@ -34,7 +36,7 @@ def is_palindrome3(s):
   Use slicing (with step being -1) to get the reverse of a
   string, and a string is a palindrome if it is equivalent to its reverse.
   """
-  return True
+  return s[::-1] == s 
 
 def run():
   s = input("Enter a string: ")
